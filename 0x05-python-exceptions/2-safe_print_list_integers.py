8f9fced88431d3edcd2abd213c1619e8ee927ddf
yadi09 <terefeyadamzer@gmail.com>
2024-02-03 15:28:08 +0100

    ..

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    idx = 0
+    i = 0
+
+    while idx < x:
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            idx += 1
+            i += 1
+        except IndexError:
+            print()
+            return i
+        except Exception:
+            idx += 1
+    print()
+    return i
