56e4cf0a84c94640acd2a566b66842bfc9f684c0
yadi09 <terefeyadamzer@gmail.com>
2024-02-24 19:25:09 +0100

    ..

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,14 +4,33 @@ import unittest
 
 class test_id(unittest.TestCase):
 
-    def test_Base(self):
-        obj = Base()
+    def test_no_arg(self):
+        obj1 = Base()
+        obj2 = Base()
+        self.assertEqual(obj1.id, obj2.id - 1)
+
+    def test_scope(self):
+        obj1 = Base(12)
+        obj1.id = 2
+        self.assertEqual(obj1.id, 2)
+
+    def test_many_id(self):
+        obj1 = Base()
         obj2 = Base()
         obj3 = Base()
-        obj4 = Base(12)
-        obj5 = Base()
-        self.assertEqual(obj.id, 1)
-        self.assertEqual(obj2.id, 2)
-        self.assertEqual(obj3.id, 3)
-        self.assertEqual(obj4.id, 12)
-        self.assertEqual(obj5.id, 4)
+        self.assertEqual(obj1.id, obj3.id - 2)
+
+    def test_None_arg(self):
+        obj1 = Base(None)
+        obj2 = Base(None)
+        self.assertEqual(obj1.id, obj2.id - 1)
+
+    def test_with_arg(self):
+        obj1 = Base(12)
+        self.assertEqual(obj1.id, 12)
+
+    def test_mix(self):
+        obj1 = Base()
+        obj2 = Base(12)
+        obj3 = Base()
+        self.assertEqual(obj1.id, obj3.id - 1)
