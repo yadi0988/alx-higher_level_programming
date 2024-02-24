9312c512e4eb98d3dc3f3c264567089d14364f0e
yadi09 <terefeyadamzer@gmail.com>
2024-02-24 14:45:12 +0100

    vv

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(Base):
         self.__width = width
         self.__height = height
         self.__x = x
-        self.__y = x
+        self.__y = y
         super().__init__(id)
 
     @property
