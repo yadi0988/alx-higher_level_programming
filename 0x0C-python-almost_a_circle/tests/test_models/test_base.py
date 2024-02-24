d1c84989a9a9975b3d1b5b521bcef2ef806a415d
yadi09 <terefeyadamzer@gmail.com>
2024-02-24 21:47:41 +0100

    ..

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,32 +5,32 @@ import unittest
 class test_id(unittest.TestCase):
 
     def test_no_arg(self):
-        obj1 = Base()
-        obj2 = Base()
-        self.assertEqual(obj1.id, obj2.id - 1)
+        self.obj1 = Base()
+        self.obj2 = Base()
+        self.assertEqual(self.obj1.id, self.obj2.id - 1)
 
     def test_scope(self):
-        obj1 = Base(12)
-        obj1.id = 2
-        self.assertEqual(obj1.id, 2)
+        self.obj1 = Base(12)
+        self.obj1.id = 2
+        self.assertEqual(self.obj1.id, 2)
 
     def test_many_id(self):
-        obj1 = Base()
-        obj2 = Base()
-        obj3 = Base()
-        self.assertEqual(obj1.id, obj3.id - 2)
+        self.obj1 = Base()
+        self.obj2 = Base()
+        self.obj3 = Base()
+        self.assertEqual(self.obj1.id, self.obj3.id - 2)
 
     def test_None_arg(self):
-        obj1 = Base(None)
-        obj2 = Base(None)
-        self.assertEqual(obj1.id, obj2.id - 1)
+        self.obj1 = Base(None)
+        self.obj2 = Base(None)
+        self.assertEqual(self.obj1.id, self.obj2.id - 1)
 
     def test_with_arg(self):
-        obj1 = Base(12)
-        self.assertEqual(obj1.id, 12)
+        self.obj1 = Base(12)
+        self.assertEqual(self.obj1.id, 12)
 
     def test_mix(self):
-        obj1 = Base()
-        obj2 = Base(12)
-        obj3 = Base()
-        self.assertEqual(obj1.id, obj3.id - 1)
+        self.obj1 = Base()
+        self.obj2 = Base(12)
+        self.obj3 = Base()
+        self.assertEqual(self.obj1.id, self.obj3.id - 1)
