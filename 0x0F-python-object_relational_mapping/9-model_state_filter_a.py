389ea26ccc94f87a5503c5da2ad5b038850000d3
yadi09 <terefeyadamzer@gmail.com>
2024-04-28 21:17:48 +0100

    ..

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
     script that lists all State objects from the database hbtn_0e_6_usa
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1],
         sys.argv[2],
         sys.argv[3])
