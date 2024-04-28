cacd1e4cebd4729614cb373b1a2ca1ec87ae16e4
yadi09 <terefeyadamzer@gmail.com>
2024-04-28 21:10:33 +0100

    ..

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,17 +13,17 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1],
         sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True
+        sys.argv[3])
     )
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    Query = session.query(State).order_by(State.id).first()
+    Query = session.query(State).first()
     if Query is None:
         print("Nothing")
     else:
-        print('{0}: {1}'.format(Query.id, Query.name))
+        print('{}: {}'.format(Query.id, Query.name))
 
     session.close()
